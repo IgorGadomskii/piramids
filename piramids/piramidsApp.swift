62edde3e8815db1d24bd28072b2a77c3fc42b768
Igor Gadomskii <igvgad@gmail.com>
2022-06-06 16:10:22 +0300

    Initial Commit

---
+++ piramids/piramidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  piramidsApp.swift
+//  piramids
+//
+//  Created by Macbook on 06.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct piramidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
